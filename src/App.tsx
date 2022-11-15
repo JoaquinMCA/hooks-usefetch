6cfedd60f358b5cdcf996dea0ff565772abd6428
Joaquín García Molina <joaquingm@MBP-de-Joaquin.home>
2022-11-15 01:51:14 +0100

    Fresh start test commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <p>Good luck using the custom hook useFetch!</p>
+    </div>
+  );
+}
+
+export default App;
